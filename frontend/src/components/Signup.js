d2cf7f6cf4d0303dc3a6b997de78694299bb71bd
Jogendra Pattnaik <jogendrapattnaik4@gmail.com>
2023-01-31 08:55:32 +0530

    Half of the Signup form completed

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,8 +1,12 @@
-import { FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
+import { FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack,Button } from "@chakra-ui/react";
 import {React,useState} from "react";
 
 function Signup(){
     const [name,setName]= useState();
+    const [email,setEmail]= useState();
+    const [password,setPassword]=useState();
+    const [show,setShow]= useState(false);
+    const handleClick= ()=>setShow(!show);
 
 
 
@@ -14,6 +18,29 @@ function Signup(){
             placeholder="Enter your name"
             onChange={(e)=>setName(e.target.value)}
             />
+        </FormControl>
+        <FormControl id="email" isRequired>
+            <FormLabel>Email Address</FormLabel>
+            <Input 
+            type='email'
+            placeholder="Enter Your Email Address"
+            onChange={(e)=>setEmail(e.target.value)}
+            />
+        </FormControl>
+        <FormControl id="password" isRequired>
+            <FormLabel>Password</FormLabel>
+            <InputGroup size='md'>
+                <Input
+                type={show?'text':'password'}
+                placeholder="Enter Password"
+                onChange={(e)=>setPassword(e.target.value)}
+                />
+            <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show?"Hide":"Show"}
+                </Button>
+            </InputRightElement>
+            </InputGroup>
         </FormControl>
     </VStack>
         );
